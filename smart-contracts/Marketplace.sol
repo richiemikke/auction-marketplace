8c03b46e11502dbf8bf5ce0970eea2b1c29cb2aa
FarzeenKist <farzeenkistnareddy@gmail.com>
2022-06-12 22:41:17 +0400

    Fixed errors, optimized contract and addead a few more securities

--- smart-contracts/Marketplace.sol
+++ smart-contracts/Marketplace.sol
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: GPL-3.0
 
 pragma solidity ^0.8.7;
 
@@ -9,19 +10,23 @@ import './MarketAuction.sol';
 
 
 //create Market to faciliate bidding and transfer of ownership or original items using ERC-721 NFTs
-contract Market is ERC721, ERC721URIStorage {
-    constructor() ERC721("Market", "MKT") public {}
+contract Market is ERC721, ERC721URIStorage, Ownable {
+    constructor() ERC721("Market", "MKT") {
+        foundation_address = payable(msg.sender);
+    }
     
    
     // this contract is designed to have the owner of this contract (foundation) to pay for most of the function calls
     //deployer of the contract - address of the foundation
     // (all but bid and withdraw)
-    address payable foundation_address = address(uint160(ownerOf()));
+    address payable foundation_address;
 
-    //using Counters for Counters.Counter;
-    //Counters.Counter token_ids;
+    using Counters for Counters.Counter;
+    Counters.Counter token_ids;
 
     mapping(uint => MarketAuction) public auctions;
+
+    mapping(uint => bool) isAuctioned;
 
     //item registered - check if it exists
     modifier itemRegistered(uint token_id) {
@@ -29,8 +34,10 @@ contract Market is ERC721, ERC721URIStorage {
         _;
     }
 
-    function createAuction(uint token_id) public onlyOwner {
+    function createAuction(uint token_id) public itemRegistered(token_id) onlyOwner {
+        require(!isAuctioned[token_id], "Token is already auctioned");
         auctions[token_id] = new MarketAuction(foundation_address);
+        isAuctioned[token_id] = true;
     }
 
     function getAuction(uint token_id) public view itemRegistered(token_id) returns(MarketAuction auction) {
@@ -40,18 +47,22 @@ contract Market is ERC721, ERC721URIStorage {
     //pass on URI that contains metadata about the item.  only owner can call this to register the item
     //token_ids.increment();  //create new token id.. increment by 1
     //uint token_id = token_ids.current();  //unique token_id (1 higher than previous)
-    function registerItem(string memory tokenURI) public payable onlyOwner {
-        uint _id = totalSupply();
+    function registerItem(string memory _tokenURI) public payable onlyOwner {
+        require(bytes(_tokenURI).length > 0, "Enter a valid token URI");
+        uint _id = token_ids.current();
+        token_ids.increment();
         _mint(msg.sender, _id);  //mint token and pass on id
-        _setTokenURI(_id, tokenURI);  //set URI
-        createAuction(_id);  //run create aunction function
+        _setTokenURI(_id, _tokenURI);  //set URI
+        createAuction(_id);  //run create auction function
     }
 
-    //end aunction for particular token id.  checked to see if it exists first
-    function endAuction(uint token_id) public onlyOwner itemRegistered(token_id) {
+    //end aunction for particular token id.  itemRegistered checks if token already exists
+    function endAuction(uint token_id) public payable onlyOwner itemRegistered(token_id) {
         MarketAuction auction = getAuction(token_id);
-        auction.auctionEnd();
+        (bool success) = auction.auctionEnd();
+        require(success, "Failed to end auction");
         safeTransferFrom(owner(), auction.highestBidder(), token_id);
+        isAuctioned[token_id] = false;
     }
 
     function auctionEnded(uint token_id) public view returns(bool) {
@@ -68,13 +79,38 @@ contract Market is ERC721, ERC721URIStorage {
         MarketAuction auction = auctions[token_id];
         return auction.pendingReturn(sender);
     }
+
+    function withdraw(uint token_id) public payable itemRegistered(token_id) {
+        MarketAuction auction = auctions[token_id];
+        require(auction.pendingReturn(msg.sender) > 0, "No amount to withdraw");
+        (bool success) = auction.withdraw();
+        require(success, "Withdrawal failed");
+
+    }
     
     //allows users to send in ether and send to aunction contract
     //function sould be public payable to accept ether
     function bid(uint token_id) public payable itemRegistered(token_id) {
         MarketAuction auction = auctions[token_id];
         //accepting the address from the msg.sender
-        auction.bid.value(msg.value)(msg.sender);
+        auction.bid{value: msg.value}(payable(msg.sender));
     }
+
+
+    // The following functions are overrides required by Solidity.
+
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+
 
 }
